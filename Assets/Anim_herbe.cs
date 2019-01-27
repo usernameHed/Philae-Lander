905ed199b49581dda757e9b26e5030886807cd88
Olivier ANGOUE-ONDO <angoueondoolivier@hotmail.com>
2019-01-27 16:06:17 +0100

    ajout  planete anim et texture tourbillon

---
+++ Assets/Anim_herbe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim_herbe : MonoBehaviour
+{
+   Animator anim;
+    bool isBouge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        isBouge = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(GameData.Tags.Player.ToString()))
+        {
+            anim.SetBool ("isBouge", true);
+            Invoke("UnableIsBouge", 0.2f);
+        }
+    }
+
+    private void UnableIsBouge()
+    {
+        anim.SetBool("isBouge", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
